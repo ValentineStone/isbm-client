00019d6a1ae2d5742a3d820ab34541f20f65eb39
Valentine Stone <sirValentineStone@gmail.com>
2018-05-12 01:10:02 +0500

    added LEGACY modifier before deleteion / move

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
-import { BrowserRouter, Route } from 'react-router-dom'
-import { MuiThemeProvider } from 'material-ui/styles'
-import CssBaseline from 'material-ui/CssBaseline'
-
-import app from '~/app'
-
-import AppHeader from './AppHeader'
-import AppNavigation from './AppNavigation'
-import AppContent from './AppContent'
-
-
-import { withTranslation, t } from '~/app/i18n'
-import RouteContext from '~/containers/RouteContext'
-
-const titleElement = document.head.querySelector('title')
-
-class App extends React.PureComponent {
-  constructor() {
-    super()
-    this.state = { theme: app.getTheme() }
-    this.onThemeChange = theme => this.setState({ theme })
-  }
-  componentDidMount() {
-    app.on('themechange', this.onThemeChange)
-  }
-  componentWillUnmount() {
-    app.off('themechange', this.onThemeChange)
-  }
+export default class App extends React.PureComponent {
   render() {
-    return (
-      <MuiThemeProvider theme={this.state.theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <Route>
-            {route =>
-              <RouteContext.Provider value={route}>
-                {ReactDOM.createPortal(t`app.name`, titleElement)}
-                <AppHeader />
-                <AppNavigation />
-                <AppContent />
-              </RouteContext.Provider>
-            }
-          </Route>
-        </BrowserRouter>
-      </MuiThemeProvider >
-    )
+    return <main>App</main>
   }
 }
-
-export default withTranslation()(App)
--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import es6Promise from 'es6-promise'
 es6Promise.polyfill()
 import 'whatwg-fetch'
 
-import app from '~/app'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from '~/components/App'
+import rootReducer from '~/reducers'
+//import { initial, translate, setTheme } from '~/actions'
+
+const store = createStore(rootReducer)
+
+const renderApp = () => ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#app')
+)
+
+store.dispatch()
