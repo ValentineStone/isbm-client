dd1f9a02ad01667ea11e73cf562ac044de87df29
Valentine Stone <sirValentineStone@gmail.com>
2018-05-15 02:42:03 +0500

    deleted _LEGACY folder, no longer usefull

--- src/_LEGACY/LEGACY_app/i18n.js
+++
@@ -1,8 +0,0 @@
-import I18n, { withTranslation as _withTranslation } from '~/react-base-i18n'
-import { importLang } from '~/langs'
-
-const i18nInstance = new I18n({ importLang, defaultLang: 'en' })
-export default i18nInstance
-export const t = i18nInstance.t
-export const withTranslation =
-  (instance = i18nInstance) => Component => _withTranslation(instance)(Component)
--- src/_LEGACY/REDUX_REF.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import promiseMiddleware from 'redux-promise'
-import thunkMiddleware from 'redux-thunk'
-import { createAction, handleActions, combineAction } from 'redux-actions'
-import reduceReducers from 'reduce-reducers'
-import { connect } from 'react-redux'
